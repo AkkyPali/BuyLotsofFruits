0d35fb205804b83ce1f834bfb168b1612b49eca4
AkkyPali <21275064+AkkyPali@users.noreply.github.com>
2021-02-12 15:16:34 -0800

    Add files via upload

---
+++ csc665-0-paliwal-921427283/buyLotsOfFruit.py
@@ -0,0 +1,52 @@
+# buyLotsOfFruit.py
+# -----------------
+# Licensing Information:  You are free to use or extend these projects for
+# educational purposes provided that (1) you do not distribute or publish
+# solutions, (2) you retain this notice, and (3) you provide clear
+# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
+# 
+# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
+# The core projects and autograders were primarily created by John DeNero
+# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
+# Student side autograding was added by Brad Miller, Nick Hay, and
+# Pieter Abbeel (pabbeel@cs.berkeley.edu).
+
+
+"""
+To run this script, type
+
+  python buyLotsOfFruit.py
+
+Once you have correctly implemented the buyLotsOfFruit function,
+the script should produce the output:
+
+Cost of [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)] is 12.25
+"""
+
+from __future__ import print_function
+
+fruitPrices = {'apples': 2.00, 'oranges': 1.50, 'pears': 1.75,
+               'limes': 0.75, 'strawberries': 1.00}
+
+
+def buyLotsOfFruit(orderList):
+    """
+        orderList: List of (fruit, numPounds) tuples
+
+    Returns cost of order
+    """
+    #initialise the variable to be updated with each for loop iteration
+    totalCost = 0.0 
+    # pull fruit name and it's quanity from the orderlist
+    for key, value in orderList: 
+            # use fruit name to pull it's price per pound from the dictionary & multiply with quanity to calculate the cost of the fruit & update the totalCost value
+            totalCost = totalCost + fruitPrices[key]* value 
+    #Return the updated value of totalCost        
+    return totalCost 
+    
+
+# Main Method
+if __name__ == '__main__':
+    "This code runs when you invoke the script from the command line"
+    orderList = [('apples', 2.0), ('pears', 3.0), ('limes', 4.0)]
+    print('Cost of', orderList, 'is', buyLotsOfFruit(orderList))
---
+++ csc665-0-paliwal-921427283/readme.txt
@@ -0,0 +1,29 @@
+﻿Name: Akanksha Paliwal
+SFSU ID: 921427283
+
+
+
+
+Addition.py
+
+
+* Use ‘+’ operation to add the arguments a, b. They could be integers or strings
+* Return a+ b. 
+
+
+buyLotsOfFruit.py
+
+
+* totalCost variable is declared and initialised with 0.00 value.
+* for loop is used on orderlist to iterate on fruit name and quantity. 
+* Use the name to pull out it’s price per pound from the dictionary and multiply with quanity. This is then added to the totalCost variable and it is updated. This is repeated for all fruits in the orderList. 
+* Outside for loop return totalCost to be used to print the final output. 
+
+
+shopSmart.py
+
+
+* To solve this problem,  two variables were initialized, min_price - to store minimum price value and min_price_shop to store the name of the store with min price.
+* Implement for loop on fruitShops and pass orderlist as an argument to iterate on fruitShop and calculate price for each shop
+* Use If condition in each iteration to compare the price with the min_price & each time the price was lower, it updated the two variables. 
+* Outside for loop return the min_price_shop so that it could be used in printing the final output
